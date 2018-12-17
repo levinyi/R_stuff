8e3013c70c59f7beecee56685ea67c92d2abc98e
dushiyi <dushiyi319@163.com>
2018-12-17 15:37:01 +0800

    some changes

--- venn.R
+++ venn.R
@@ -5,10 +5,8 @@ A = read.table(args[1], header=F)
 # A
 B = read.table(args[2], header=F)
 # B
-# type(B)
+
 A = as.vector(A)
 B = as.vector(B)
-A
-# venn.diagram(list(A=args[1],B=args[2]),height=500, width=500, resolution=500, filename="VennPlot.tiff", imagetype="tiff", col="transparent", )
-venn.diagram(list(A=A, B=B),height=500, width=500, resolution = 300, imagetype = "png", alpha=c(0.5,0.5), fill=c("red","yellow"), main="Intersection of xxx",filename = "VennDiagram.png")
-# venn.diagram(list(A=c(1:10), B=c(5:15)),height=500, width=500, resolution = 300, imagetype = "png", alpha=c(0.5,0.5), fill=c("red","yellow"), main="Intersection of xxx",filename = "VennDiagram.png")
+
+venn.diagram(list(A=A, B=B), height=500, width=500, resolution = 300, imagetype = "png", alpha=c(0.5,0.5), fill=c("red","yellow"), main="Intersection of xxx", filename = "VennDiagram.png")
