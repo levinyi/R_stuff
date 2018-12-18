5918470d3febca3ad5875e5e2b71e3083f5df60e
dushiyi <dushiyi319@163.com>
2018-12-18 16:47:25 +0800

    some changes

--- igblast.result.visualization.R
+++ igblast.result.visualization.R
@@ -6,14 +6,19 @@ library(ggplot2)
 
 # identity
 # mv Rplots.pdf Rplots.identity.pdf
-ggplot(data, aes(x=symbol, y=identity)) + geom_violin()
+#ggplot(data, aes(x=symbol, y=identity)) + geom_violin()
 
 # length
 # mv Rplots.pdf Rplots.length.pdf
-ggplot(data, aes(x=symbol, y=length)) + geom_violin()
+#ggplot(data, aes(x=symbol, y=length)) + geom_violin()
 
 # evalue 
 # mv Rplots.pdf Rplots.evalue.pdf
-ggplot(data, aes(x=symbol, y=log(evalue))) + geom_violin()
+#ggplot(data, aes(x=symbol, y=log(evalue))) + geom_violin()
 
+# length and e-value
 
+#ggplot(data, aes(x=evalue, y=length, colour=symbol)) + geom_point(alpha=.4)
+data2 = subset(data, evalue >1e-10)
+ggplot(data2, aes(x=evalue, y=length, colour=symbol)) + geom_point(alpha=.4)
+ggsave("myplot.png", width=8, height=8, unit="cm", dpi=300)
