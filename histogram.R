519aa4c33aa6efc2c1dfa15176a90bd7a9746da0
dushiyi <dushiyi319@163.com>
2019-07-09 16:03:27 +0800

    some changes

--- histogram.R
+++ histogram.R
@@ -4,6 +4,14 @@ data = read.table(args[1],header=F)
 #head(data)
 #new_data = data[data$V3>=3,]
 #head(new_data)
-ggplot(data,aes(x=data$V2)) + geom_histogram(aes(y =..count..), binwidth=1)
+ggplot(data,aes(x=data$V2)) + 
+	geom_histogram(aes(y =..count..), binwidth=1)+
+	xlab("") +
+	scale_x_continuous(limits=c(0,100),breaks=seq(0,100,1)) +
+	theme(
+	      panel.background = element_blank(),
+	      axis.line = element_line(),
+	      panel.border = element_rect(fill=NA)
+	      )
 ggsave("histogram.jpg")
 print("output is : histogram.jpg")
