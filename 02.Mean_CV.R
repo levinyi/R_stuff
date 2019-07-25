0faf61bdaf38561ce22ffec538f64aa043a67047
dushiyi <dushiyi319@163.com>
2019-07-25 10:18:20 +0800

    rm some files

--- 02.Mean_CV.R
+++
@@ -1,5 +0,0 @@
-args=commandArgs(T)
-data = read.table(args[1],sep=',',header=TRUE)
-
-x=data[,2]
-y=data[,3]
--- trend_line.R
+++
@@ -1,4 +0,0 @@
-xz = c(2,4,6,7,9)
-yz = c(11,13,14,15)
-plot(xz,yz)
-lines(lowess(xz,yz))
