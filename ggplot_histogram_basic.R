5291bc7aaa641f7f64ca286d51c06efec945d586
dushiyi <dushiyi319@163.com>
2019-08-14 09:11:31 +0800

    some changes

--- ggplot_histogram_basic.R
+++ ggplot_histogram_basic.R
@@ -4,14 +4,17 @@ data = read.table(args[1],header=F)
 #head(data)
 #new_data = data[data$V3>=3,]
 #head(new_data)
+
 ggplot(data,aes(x=data$V2)) + 
-	geom_histogram(aes(y =..count..), binwidth=1)+
+	geom_histogram(aes(y =..count..), binwidth=1) +
 	xlab("") +
-	scale_x_continuous(limits=c(0,max(data$V2)),breaks=seq(0,100,1)) +
+	scale_x_continuous(limits=c(0, max(data$V2)), breaks=seq(0,100,1)) +
+	
 	theme(
 	      panel.background = element_blank(),
 	      axis.line = element_line(),
 	      panel.border = element_rect(fill=NA)
-	      )
+	)
+
 ggsave("Basic_histogram.jpg")
 print("output is : Basic_histogram.jpg")
