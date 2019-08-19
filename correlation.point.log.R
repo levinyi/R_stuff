88e97f22e29386b985c9c8542d71760ba2b79e06
dushiyi <dushiyi319@163.com>
2019-08-19 18:49:09 +0800

    some changes

--- correlation.point.log.R
+++ correlation.point.log.R
@@ -9,15 +9,17 @@ for (each_file in args) {
         #cor(x=log(data[,2],10),y=log(data[,3],10),method="spearman"
     c = cor(data[,2],data[,3],method="pearson") # person coefficient of association
     d = cor(data[,2],data[,3],method="spearman") # spearman coefficient of association
-    ggplot(data, aes(log(data[,2],10), log(data[,3],10))) +
+    data[,2] = data[,2] + 1 # log(1,2) = 0
+    data[,3] = data[,3] + 1 # log(1,2) = 0
+    ggplot(data, aes(log(data[,2], 2), log(data[,3],2))) +
         geom_point(size=1.5) + # default size is 2
         #geom_smooth(method=lm, se=FALSE) +
 	geom_abline()+ # line: y=x
         xlab(names(data)[2]) + ylab(names(data)[3]) +
     	annotate("text",x=-Inf,y= Inf,hjust=0,vjust=0.99,label=paste(paste( "pearson", round(c,4), sep=":"),paste("spearman", round(d,4), sep=":"),sep=("\n"))) +
 	#xlim(min(log(data[,2],10)),max(log(data[,2],10))) +
-	scale_x_continuous(limits=c(0,max(log(data[,2],10))))+
-	scale_y_continuous(limits=c(0,max(log(data[,3],10))))+
+	scale_x_continuous(limits=c(0, max(log(data[,2],2))))+
+	scale_y_continuous(limits=c(0, max(log(data[,3],2))))+
 	theme(panel.background=element_blank(),
 	      panel.border=element_rect(fill=NA),
 	      axis.line=element_line()
