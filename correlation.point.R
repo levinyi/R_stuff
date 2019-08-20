d0280c208a6a3b227a863e9d91756e48abc04f8e
dushiyi <dushiyi319@163.com>
2019-08-20 18:24:44 +0800

    some changes

--- correlation.point.R
+++ correlation.point.R
@@ -9,6 +9,11 @@ for (each_file in args) {
         #cor(x=log(data[,2],10),y=log(data[,3],10),method="spearman"
     c = cor(data[,2],data[,3],method="pearson") # person coefficient of association
     d = cor(data[,2],data[,3],method="spearman") # spearman coefficient of association
+    if(max(data[,2])>max(data[,3])){
+	    max_scale = max(data[,2])
+    }else{
+	    max_scale = max(data[,3])
+    }
     ggplot(data, aes(data[,2], data[,3])) +
         geom_point(size=1.5) + # default size is 2
         #geom_smooth(method=lm, se=FALSE) + # trend line
@@ -16,15 +21,15 @@ for (each_file in args) {
         xlab(names(data)[2]) + ylab(names(data)[3]) +
     	annotate("text",x=-Inf,y= Inf,hjust=0,vjust=0.99,label=paste(paste( "pearson", round(c,4), sep=":"),paste("spearman", round(d,4), sep=":"),sep=("\n"))) +
 	#xlim(min(data[,2]),max(data[,2])) +
-	scale_x_continuous(limits=c(0,max(data[,2])))+
-	scale_y_continuous(limits=c(0,max(data[,3])))+
+	scale_x_continuous(limits=c(0,max_scale))+
+	scale_y_continuous(limits=c(0,max_scale))+
 	theme(panel.background = element_blank(),
 	      panel.border = element_rect(fill=NA),
 	      axis.line=element_line())
 	#annotate("text",x=-Inf,y= Inf,hjust=0,vjust=1,label=paste("spearman", d, sep=":"))
     outputname = paste(name, index, "corelation.jpg", sep=".")
     #outputname = paste("03",as.character(index),"corelation.pdf",sep=".")
-    #print(outputname)
+    print(outputname)
     ggsave(outputname)
     index = index + 1
     
