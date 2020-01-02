a4eedae4264577382f99f9cca8f854c72f36c95c
dushiyi <dushiyi319@163.com>
2020-01-02 09:46:20 +0800

    modified ggplot

--- ggplot_histogram_basic.R
+++ ggplot_histogram_basic.R
@@ -1,15 +1,22 @@
 library(ggplot2)
 args=commandArgs(T)
+
 data = read.table(args[1],header=F)
 value_column = as.integer(args[2])
 mode(value_column)
+
 colnames(data)[value_column] <- "value"
 head(data)
+
+min_data = min(data$value)
+max_data = max(data$value)
+
 ggplot(data,aes(x=data$value)) + 
 	geom_histogram(aes(y =..count..), binwidth=1) +
 	#geom_bar(aes(y =..count..)) +
 	xlab("") +
-	scale_x_continuous(limits=c(0, max(data$value)), breaks=seq(0,100,1)) +
+	# scale_x_continuous(limits=c(min_data, max_data), breaks=seq(min_data,max_data,1)) +
+	scale_x_continuous(limits=c(min_data, max_data)) +
 	
 	theme(
 	      panel.background = element_blank(),
